d72f46747c5fb824b3fb02b3f1bb2e39041490de
zxp <zxp>
2016-10-19 17:36:52 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("args = [" + args + "]");
+        int i=1000;
+        for (int j = 0; j < i; j++) {
+            System.out.println("j = " + j);
+            System.out.println("j = " + j);
+        }
+    }
+}
---
+++ src/sss.java
@@ -0,0 +1,18 @@
+/**
+ * Created by zhangxiaoping on 2016-10-19.
+ */
+
+/**
+ * @author zhangxiaoping
+ * @FileName sss.java
+ * @Date 2016-10-19 17:17
+ * @since JDK 1.7
+ */
+public class sss {
+    public static void main(String[] args) {
+        System.out.println("sss.main");
+        System.out.printf("");
+        System.out.println("args = [" + args + "]");
+        System.out.println("args = " + args);
+    }
+}
